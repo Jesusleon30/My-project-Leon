a6261a87519ce1ae58713fe5fd99901d9f37fe31
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-21 21:34:56 +0200

    nuovo progetto

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+// document.querySelector("button")
+//     .onclick = function(){
+//         alert("hola mundo")
+//     }
+
+
+
+
+
+// let nombre = "Jesus";
+// console.log("nombre:", nombre)
+
+// let numero1 = "1"
+// let numero2 = "2"
+// let suma = parseInt(numero1) + parseInt(numero2)
+// console.log(suma)
+
+// funciones 
+// function suma(a, b){
+//     return a + b 
+// }
+
+// console.log(suma(2, 3))
+
+
+
+// function sumar(a, b) {
+//     return a - b
+
+// }
+
+// let suma = sumar
+
+// console.log(suma(2, 3))
+
+
+// function sumaBase(x){
+//     return function(y){
+//         return x + y
+//     }
+// }
+
+// const sumabase5 = sumaBase(5)
+
+// console.log(sumabase5(4))
+
+
+// function count(){
+//     let count = 0 
+//     return function(){
+//         return count = count + 1 
+//     }
+// }
+
+// let count1 = count()
+
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+
