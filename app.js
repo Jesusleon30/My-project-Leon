0539edafce9d86c12f88d32f419d808cfe75fb13
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-21 23:04:02 +0200

    javascript button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+// modo onclick:
 // document.querySelector("button")
 //     .onclick = function(){
 //         alert("hola mundo")
@@ -7,56 +7,22 @@
 
 
 
+const btnAumentar = document.querySelector(".btnMas")
+const span = document.getElementById("span2")
+let contador = 0 
 
+btnAumentar.addEventListener("click", () =>{
+    console.log('un click de aumentar')
+    contador++
+    span.textContent = contador
+    
+})
 
-// let nombre = "Jesus";
-// console.log("nombre:", nombre)
+const btnDiminuir = document.querySelector(".btnMenos")
 
-// let numero1 = "1"
-// let numero2 = "2"
-// let suma = parseInt(numero1) + parseInt(numero2)
-// console.log(suma)
-
-// funciones 
-// function suma(a, b){
-//     return a + b 
-// }
-
-// console.log(suma(2, 3))
-
-
-
-// function sumar(a, b) {
-//     return a - b
-
-// }
-
-// let suma = sumar
-
-// console.log(suma(2, 3))
-
-
-// function sumaBase(x){
-//     return function(y){
-//         return x + y
-//     }
-// }
-
-// const sumabase5 = sumaBase(5)
-
-// console.log(sumabase5(4))
-
-
-// function count(){
-//     let count = 0 
-//     return function(){
-//         return count = count + 1 
-//     }
-// }
-
-// let count1 = count()
-
-// console.log(count1())
-// console.log(count1())
-// console.log(count1())
+btnDiminuir.addEventListener("click", () => {
+    console.log("otro click")
+    contador = contador - 1
+    span.textContent = contador
+})
 
