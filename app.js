ae971d68050414c57d914d24c6ac1f0509b464fa
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-22 11:11:10 +0200

    evento click

--- app.js
+++ app.js
@@ -1,28 +1,76 @@
 
+
+/** 
 // modo onclick:
-// document.querySelector("button")
-//     .onclick = function(){
-//         alert("hola mundo")
-//     }
+document.querySelector("button")
+    .onclick = function(){
+        alert("hola mundo")
+    }
+*/
 
 
 
-const btnAumentar = document.querySelector(".btnMas")
+// modo normal:
+const btnAumentar = document.querySelector(".btn-info") //("btnMas")
 const span = document.getElementById("span2")
 let contador = 0 
 
 btnAumentar.addEventListener("click", () =>{
-    console.log('un click de aumentar')
+    console.log('un click para aumentar')
     contador++
     span.textContent = contador
     
 })
 
-const btnDiminuir = document.querySelector(".btnMenos")
+const btnDiminuir = document.querySelector(".btn-danger") //("btnMenos")
 
 btnDiminuir.addEventListener("click", () => {
-    console.log("otro click")
-    contador = contador - 1
+    console.log("otro click para diminuir")
+    contador--
+    //otro modo:
+    //contador = contador - 1
     span.textContent = contador
 })
+ 
 
+
+
+/** 
+// modo EVENT DELEGATION:
+
+//
+const container = document.querySelector(".container")
+const span = document.getElementById("span2")
+let contador = 0 
+
+// de este modo delegamos eventos en todo el container
+//nosotros estamos haciendo el evento click de todo el container
+
+container.addEventListener("click", (e) =>{
+    //console.log(e.target)
+    console.log(e.target.classList.contains("btn-info")) 
+    if(e.target.classList.contains("btn-info")){
+        contador++
+        span.textContent = contador
+    }
+    if(e.target.classList.contains("btn-danger"))
+        {
+        contador--
+        span.textContent = contador
+    }
+})
+*/
+
+
+
+// e.stopPropagation() 
+// solo si ejecuto el click en el boton se ese se ejecuta en cuestion y no se propaga
+const btn = document.querySelector(".btn-dark")
+const bgSuccess = document.querySelector(".bg-success")
+
+btn.addEventListener("click", (e) => {
+    console.log("click boton")
+    e.stopPropagation()
+})
+
+bgSuccess.addEventListener("click", () =>{console.log("click bgSuccess")})
